8eed5c88c2629cf67531cfc051b8af0d21fbe066
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-02-18 22:34:52 -0600

    chmod to setup xcode file and added feedback to setupnvim

--- setup_nvim.zsh
+++ setup_nvim.zsh
@@ -2,6 +2,7 @@
 
 echo "\n <<< Starting Vim Setup >>>\n"
 mv ~/.config/init.vim ~/.config/nvim/init.vim
+echo "\n <<< moved the file! >>>\n"
 
 echo "\n <<< Installing Vim-Plug (Plugin manager) >>>\n"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
